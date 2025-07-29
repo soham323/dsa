c6be7521f1bbd7911ec131d11f7d7f696f3f9e09
Soham <118099505+soham323@users.noreply.github.com>
2025-07-29 08:23:15 +0530

    code logs for better understanding

--- Binary-search/peak-mountain-array.cpp
+++ Binary-search/peak-mountain-array.cpp
@@ -8,12 +8,16 @@ int main(){
     int mid  = start + (end -start) / 2;
     int peak = -1;
     while(start<end){
+        cout << "Start: " << start << " End: " << end << " Mid: " << mid << endl;
         if(arr[mid]<arr[mid+1]){
+            cout << "Mid is less than next element, moving start to mid + 1" << endl;
             start = mid + 1;
         }
         else {
+            cout << "Mid is greater than or equal to next element, moving end to mid" << endl;
             end = mid;
         }
+        cout << "New Start: " << start << " New End: " << end << endl;
         mid = start + (end - start) / 2;
     }
     peak = arr[start];
